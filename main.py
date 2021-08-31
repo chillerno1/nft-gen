6e8a99a4b9c1b7e31e835641c8e0e2fa80eb6cee
Anders Magnusson <anders14magnusson@gmail.com>
2021-08-31 03:25:51 +0200

    feat: Generate multiple

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from generate import generate
+from settings import config
 
 
 def main():
-    nft = generate()
-
-    print(nft.attributes)
-    nft.image.show()
+    for n in range(1):
+        nft = generate()
+        print(f"{n}: {nft.attributes}")
+        nft.image.save(f"{config.output_dir}/{n}.png")
 
 
 if __name__ == '__main__':
